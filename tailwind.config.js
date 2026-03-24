2c28c366ba0eaf412b8662b6be5d086d68b8cf97
Macwilson007 <wilsonigiri@gmail.com>
2026-03-24 21:52:00 +0100

    style: increase global font hierarchy by 4px

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      xs: ['1rem', { lineHeight: '1.25rem' }],
+      sm: ['1.125rem', { lineHeight: '1.5rem' }],
+      base: ['1.25rem', { lineHeight: '1.75rem' }],
+      lg: ['1.375rem', { lineHeight: '1.875rem' }],
+      xl: ['1.5rem', { lineHeight: '2rem' }],
+      '2xl': ['1.75rem', { lineHeight: '2.25rem' }],
+      '3xl': ['2.125rem', { lineHeight: '2.5rem' }],
+      '4xl': ['2.5rem', { lineHeight: '2.75rem' }],
+      '5xl': ['3.25rem', { lineHeight: '1' }],
+      '6xl': ['4rem', { lineHeight: '1' }],
+      '7xl': ['4.75rem', { lineHeight: '1' }],
+      '8xl': ['6.25rem', { lineHeight: '1' }],
+      '9xl': ['8.25rem', { lineHeight: '1' }],
+    },
     extend: {
       colors: {
         onyx: '#121418',
